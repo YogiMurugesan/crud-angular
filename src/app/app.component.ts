2a963df346052da1847ac44ecb84e0da5bcf0dcd
Yogeswari <yogeswari1805@gmail.com>
2024-11-16 11:45:58 +0530

    add-row

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  
+  imports: [RouterOutlet,HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'crud-angular';
+  users:any[]=[]
+
+  constructor(private http: HttpClient){}
+  ngOnInit(){
+    this.http.get<any[]>('http://localhost:3000/users')
+    .subscribe({
+      next:(users:any[])=>{
+        this.users=users;
+      }
+    });
+  }
 }
